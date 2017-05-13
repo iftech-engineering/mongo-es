c448e89f4c48c020c6aff178af33e0db97ac7ed5
RenzHoly <renzholy@gmail.com>
2017-05-13 17:32:45 +0800

    fix: applyUpdate

--- src/transform.ts
+++ src/transform.ts
@@ -1,8 +1,8 @@
-import { forEach, size, get, set, has, keys } from 'lodash'
+import { forEach, size, get, set, unset, has, keys } from 'lodash'
 import { Task, Document, OpLog, IntermediateRepresentation, ObjectId } from './types'
 import { mongodb, elasticsearch } from './models'
 
-function transformer(action: 'create' | 'update' | 'delete', task: Task, doc: Document): IntermediateRepresentation | null {
+function transformer(task: Task, action: 'create' | 'update' | 'delete', doc: Document): IntermediateRepresentation | null {
   const IR: IntermediateRepresentation = {
     action,
     id: doc._id.toHexString(),
@@ -21,6 +21,18 @@ function transformer(action: 'create' | 'update' | 'delete', task: Task, doc: Do
     return null
   }
   return IR
+}
+
+function applyUpdate(task: Task, doc: Document, $set: any = {}, $unset: any = {}): Document {
+  forEach(task.transform.mapping, (value, key) => {
+    if (has($unset, key)) {
+      unset(doc, value)
+    }
+    if (has($set, key)) {
+      set(doc, value, get($set, key))
+    }
+  })
+  return doc
 }
 
 function ignoreUpdate(task: Task, oplog: OpLog): boolean {
@@ -95,14 +107,14 @@ async function retrieveFromElasticsearch(task: Task, id: ObjectId): Promise<Docu
 }
 
 export function document(task: Task, doc: Document): IntermediateRepresentation | null {
-  return transformer('create', task, doc)
+  return transformer(task, 'create', doc)
 }
 
 export async function oplog(task: Task, oplog: OpLog): Promise<IntermediateRepresentation | null> {
   try {
     switch (oplog.op) {
       case 'i': {
-        return transformer('create', task, oplog.o)
+        return transformer(task, 'create', oplog.o)
       }
       case 'u': {
         if (size(oplog.o2) !== 1 || !oplog.o2._id) {
@@ -114,16 +126,16 @@ export async function oplog(task: Task, oplog: OpLog): Promise<IntermediateRepre
           return null
         }
         if (keys(oplog.o).filter(key => key.startsWith('$')).length === 0) {
-          return transformer('update', task, {
+          return transformer(task, 'update', {
             _id: oplog.o2._id,
             ...oplog.o,
           })
         }
-        const doc = (task.transform.parent
+        const old = task.transform.parent
           ? await searchFromElasticsearch(task, oplog.o2._id)
           : await retrieveFromElasticsearch(task, oplog.o2._id)
-        ) || await retrieveFromMongoDB(task, oplog.o2._id)
-        return doc ? transformer('update', task, doc) : null
+        const doc = old ? applyUpdate(task, old, old.$set, old.$unset) : await retrieveFromMongoDB(task, oplog.o2._id)
+        return doc ? transformer(task, 'update', doc) : null
       }
       case 'd': {
         if (size(oplog.o) !== 1 || !oplog.o._id) {
@@ -134,7 +146,7 @@ export async function oplog(task: Task, oplog: OpLog): Promise<IntermediateRepre
           ? await retrieveFromElasticsearch(task, oplog.o._id)
           : oplog.o
         console.debug(doc)
-        return doc ? transformer('delete', task, doc) : null
+        return doc ? transformer(task, 'delete', doc) : null
       }
       default: {
         return null
