03d740c4e5e3df3fd3ccd760c08159f38bd5072d
RenzHoly <renzholy@gmail.com>
2017-05-17 14:54:13 +0800

    fix

--- src/models.ts
+++ src/models.ts
@@ -7,25 +7,25 @@ import { Config, ExtractTask, MongoDB, Elasticsearch } from './types'
 let _mongodb: MongoDB
 let _elasticsearch: Elasticsearch
 
-function key(obj: any): string {
-  return createHash('md5').update(JSON.stringify(key)).digest('hex')
+function hash(obj: any): string {
+  return createHash('md5').update(JSON.stringify(obj)).digest('hex')
 }
 
 export async function init(config: Config): Promise<void> {
   _mongodb = {}
   const url = parse(config.mongodb.url)
   url.pathname = `/local`
-  _mongodb[key('local')] = await MongoClient.connect(format(url), config.mongodb.options)
+  _mongodb[hash('local')] = await MongoClient.connect(format(url), config.mongodb.options)
   for (let task of config.tasks) {
     const url = parse(config.mongodb.url)
     url.pathname = `/${task.extract.db}`
-    _mongodb[key(task.extract)] = await MongoClient.connect(format(url), config.mongodb.options)
+    _mongodb[hash(task.extract)] = await MongoClient.connect(format(url), config.mongodb.options)
   }
   _elasticsearch = new Client(config.elasticsearch.options)
 }
 
 export function mongodb(task: ExtractTask | 'local') {
-  return _mongodb[key(task)]
+  return _mongodb[hash(task)]
 }
 
 export function elasticsearch() {
