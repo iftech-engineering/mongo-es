692b364312301dd83ae5d0c02c3200fd96beb6c9
任泽华 <renzholy@gmail.com>
2018-01-22 19:22:13 +0800

    3.2.2

--- src/elasticsearch.ts
+++ src/elasticsearch.ts
@@ -79,6 +79,9 @@ export default class Elasticsearch {
           const docs = response.hits.hits.map((hit: any) => {
             const doc = hit._source
             doc._id = new ObjectID(hit._id)
+            if (this.task.transform.parent && hit._parent) {
+              _.set(doc, this.task.transform.parent, new ObjectID(hit._parent))
+            }
             return doc as Document
           })
           resolve(_.keyBy(docs, doc => doc._id.toHexString()))
